166d1f00cabf74e3b4d0ec8721b888096ddeb9f3
sjones4215 <bluesfansince2013@gmail.com>
2020-09-09 18:10:14 -0500

    cards finished

--- src/app/puppylist/puppylist.component.ts
+++ src/app/puppylist/puppylist.component.ts
@@ -34,7 +34,41 @@ export class PuppylistComponent implements OnInit {
     breed: "Blue Heeler",
     age: 7,
     image: "https://cf.ltkcdn.net/dogs/images/orig/236062-1600x1060-blue-heeler-puppies.jpg"
+  },
+  { 
+    name: "Denise",
+    breed: "Blue Heeler",
+    age: 7,
+    image: "https://cf.ltkcdn.net/dogs/images/orig/236062-1600x1060-blue-heeler-puppies.jpg"
+  },
+  { 
+    name: "Denise",
+    breed: "Blue Heeler",
+    age: 7,
+    image: "https://cf.ltkcdn.net/dogs/images/orig/236062-1600x1060-blue-heeler-puppies.jpg"
+  },
+  { 
+    name: "Denise",
+    breed: "Blue Heeler",
+    age: 7,
+    image: "https://cf.ltkcdn.net/dogs/images/orig/236062-1600x1060-blue-heeler-puppies.jpg"
+  },
+
+  { 
+    name: "Denise",
+    breed: "Blue Heeler",
+    age: 7,
+    image: "https://cf.ltkcdn.net/dogs/images/orig/236062-1600x1060-blue-heeler-puppies.jpg"
+  },
+
+  { 
+    name: "Denise",
+    breed: "Blue Heeler",
+    age: 7,
+    image: "https://cf.ltkcdn.net/dogs/images/orig/236062-1600x1060-blue-heeler-puppies.jpg"
   }
+
+
 
   ]
   constructor() { }
