e0f09d96025687264f2e588746430cbbdd96f2ff
Zack Amis <zachariah.amis@vizientinc.com>
2020-09-24 20:23:57 -0500

    wow

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormControl } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
@@ -5,5 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  formGroup: FormGroup = new FormGroup({
+    wow: new FormControl('')
+  })
 
+  ngOnInit(): void {
+    var audio = new Audio();
+    audio.src = "assets/wowb.mp3";
+    this.formGroup.valueChanges.subscribe(change => {
+      audio.load();
+      audio.play();
+    })
+    
+  }
 }
